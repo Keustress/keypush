3f8a0eda36750dc6b7eadc2232e50723ba36fbfb
정지훈 <jungjihoons@naver.com>
2023-07-09 04:20:59 +0900

    #1 - Banner 컴포넌트 생성 및 스타일링
    Main Page에 나올 배너를 생성하고 기본적인 그림을 넣어 구성
    
    추후 구현
    - swiper를 이용하여 슬라이드 상황 구현
    - Firebase를 이용한 LoginPage 구현 이후 사용자 이미지 가져와서 넣기
    - 데이터 값 가져와서 슬라이드 배너에 맞게 값 구성

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import "./Banner.css";
 
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <Container>
+      <div className="banner__slide">
+        <a className="banner__item" href>
+          <img className="banner__cover" src="./images/keyboard-13.png" alt="keyboard" />
+          <div className="banner__txt">
+            <div className="banner__txt_title">게시글 제목</div>
+            <div className="banner__txt_user">작성자</div>
+            <div className="banner__user_icon">작성자 그림</div>
+          </div>
+          <div className="banner--fadeBottom" />
+        </a>
+      </div>
+    </Container>
+  );
+};
 
-export default Banner
+export default Banner;
+
+const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
+  overflow: hidden;
+  list-style: none;
+  padding: 0;
+  max-height: 100%;
+  z-index: 1;
+`;
